8017e8bdd81d7c870053df93430566a0414e2741
Henrique da Silveira <139990659+imrickss@users.noreply.github.com>
2023-08-09 12:27:57 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,19 @@
+function click() {
+    // Declarção de numeros
+    const num7 = document.getElementById('7')
+    const num4 = document.getElementById('4')
+    const num1 = document.getElementById('1')
+    const del = document.getElementById('<')
+    const num8 = document.getElementById('8')
+    const num5 = document.getElementById('5')
+    const num2 = document.getElementById('2')
+    const num0 = document.getElementById('9')
+    const num6 = document.getElementById('6')
+    const num3 = document.getElementById('3')
+    const comma = document.getElementById('virgula')
+    const div = document.getElementById('div')
+    const multiplicar = document.getElementById('mul')
+    const subtrair = document.getElementById('sub')
+    const soma = document.getElementById('som')
+    const igual = document.getElementById('igual')
+}
